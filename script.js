f1751006896d4b5d0ea981a13dd50d2e16317020
Glitch (hello-webpage) <none>
2020-03-04 23:04:25 +0000

    🍖⚾️ Checkpoint ./script.js:11792803/757

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ fetch(
   "http://api.openweathermap.org/data/2.5/forecast?lat=-34.6230&lon=-58.4015&appid=e82888e26a307e41dd4e36764d317c4f"
 )
   .then(function(response) {
-    return response.text();
-  
+    return response.json();
   })
   .then(function(myJson) {
     document.getElementById("var").innerHTML = "<p> myJson </p>";
