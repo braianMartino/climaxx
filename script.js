08050289c3b3c1b68ab3666930177d077aca5121
Glitch (hello-webpage) <none>
2020-03-04 11:59:26 +0000

    🏌✈️ Checkpoint ./script.js:11792803/639 ./index.html:11792803/11

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-// prints "hi" in the browser's dev tools console
+fetch('http://api.openweathermap.org/data/2.5/forecast?lat=-34.6230&lon=-58.4015&appid=e82888e26a307e41dd4e36764d317c4f')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(myJson);
+  });
 console.log("hi");
+
+document.getElementById("var").innerHTML = "<p> hola </p>";
